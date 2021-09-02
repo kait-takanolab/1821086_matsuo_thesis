45e056d29201b442893d1bf9088f33af4fe9251d
yusuke-matsuo <79350406+yusuke-matsuo-0430@users.noreply.github.com>
2021-09-03 07:08:54 +0900

    webget説明追加とtargetをラズパイに変更

--- program/WEBサーバに意図的に負荷をかけるシェル/wgetのバッチファイル.bat
+++ program/WEBサーバに意図的に負荷をかけるシェル/wgetのバッチファイル.bat
@@ -1,20 +1,26 @@
 rem D評価サーバを作るため、意図的にサイトに負荷をかける。
 rem Windowsでは標準でWGETに代わるbitsadmin /TRANSFERが用意されていた。
 rem Dos攻撃みたいになるのでlocal以外で使わないでね。
-rem bitsadmin.exe /TRANSFER <ジョブ名：任意でOK> <リモートURL> <ダウンロード先>
-rem 別のwinコンピュータで動くように環境変数を利用した
+
+
+rem どのサーバに負荷をかけるか選択↓
+set target=http://192.168.1.81/index.php
+:set target=http://192.168.1.82/index.php
+:set target=http://192.168.1.83/index.php
+
+rem 別のwinコンピュータで動くように環境変数を利用した。
 rem 環境変数%HOMEPATH%は「\Users\ユーザー名」を自動で選んでくれる。
-rem 同一ファイル名だと上書きになって分かりにくいのでloopに使う変数でファイル名を変えるようにした。1.html,2.html…
-
-
-
-
 set forward=C:%HOMEPATH%\Downloads\
+
+
 for /l %%n in (1,1,10) do (
 
-bitsadmin /TRANSFER tes http://kumasakasoukou.com/index.html %forward%%%n.html
+rem bitsadmin.exe /TRANSFER <ジョブ名：任意でOK> <リモートURL> <ダウンロード先>
+rem 同一ファイル名だと上書きになって分かりにくいのでloopに使う変数でファイル名を変えるようにした。1.html,2.html…
+bitsadmin /TRANSFER tes %target% %forward%%%n.php
 
 )
 
+rem 結果が見れるように
 pause
 

e0f690b17a046245ae791eddf098132afc5b4d28
yusuke-matsuo <79350406+yusuke-matsuo-0430@users.noreply.github.com>
2021-09-03 07:04:32 +0900

    Update wgetのバッチファイル.bat

--- program/WEBサーバに意図的に負荷をかけるシェル/wgetのバッチファイル.bat
+++ program/WEBサーバに意図的に負荷をかけるシェル/wgetのバッチファイル.bat
@@ -1,15 +1,20 @@
 rem D評価サーバを作るため、意図的にサイトに負荷をかける。
 rem Windowsでは標準でWGETに代わるbitsadmin /TRANSFERが用意されていた。
 rem Dos攻撃みたいになるのでlocal以外で使わないでね。
-
 rem bitsadmin.exe /TRANSFER <ジョブ名：任意でOK> <リモートURL> <ダウンロード先>
+rem 別のwinコンピュータで動くように環境変数を利用した
+rem 環境変数%HOMEPATH%は「\Users\ユーザー名」を自動で選んでくれる。
+rem 同一ファイル名だと上書きになって分かりにくいのでloopに使う変数でファイル名を変えるようにした。1.html,2.html…
 
 
-rem %HOMEPATH%は環境変数「\Users\ユーザー名」をコンピュータ毎に変えてくれる
 
 
-:for /l %%n in (1,1,100000) do (
-:bitsadmin /TRANSFER tes http://kumasakasoukou.com/index.html C:%HOMEPATH%\Downloads\index.html
-:)
+set forward=C:%HOMEPATH%\Downloads\
+for /l %%n in (1,1,10) do (
 
+bitsadmin /TRANSFER tes http://kumasakasoukou.com/index.html %forward%%%n.html
+
+)
+
+pause
 

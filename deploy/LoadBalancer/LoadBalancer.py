44cc2c81b4323a91393b3dc23032350ae146f81c
yusuke-matsuo <s1821086@cce.kanagawa-it.ac.jp>
2021-11-02 21:45:04 +0900

    カウント維持するようにプログラムを改良
    NGINXのカウントをリセットがされないようにプログラムを改良しました。かなり冗長コードがありますがとりあえず動きました。

--- deploy/LoadBalancer/LoadBalancer.py
+++ deploy/LoadBalancer/LoadBalancer.py
@@ -56,8 +56,9 @@ minmam = min(list1[0],list2[0],list3[0])
 
 #--------------コンフィグ書き換え----------------
 
-#既に決まっているところはそのまま書くよ。
 f = open('/etc/nginx/nginx.conf', 'w', encoding='UTF-8')
+
+#既に決まっているところはそのまま書くよ。
 datalist1 = ['user www-data;\n','worker_processes auto;\n','pid /run/nginx.pid;\n','include /etc/nginx/modules-enabled/*.conf;\n','events {\n','	worker_connections 768;\n','}\n','http {\n','	include /etc/nginx/mime.types;\n','gzip on;\n','gzip_vary on;\n','upstream backend1{\n']
 f.writelines(datalist1)
 
@@ -65,46 +66,21 @@ if minmam == list1[0]:#81が平均最速なら
     print('\n過去24時間で「サーバ81」が一番応答速度が速いので優先接続されます。')
     datalist2 = ['server 192.168.1.81 weight=10;\n server 192.168.1.82 weight=1;\n server 192.168.1.83 weight=1;\n']
     f.writelines(datalist2)
+    allocation='192.168.1.81'
     
     
 elif minmam == list2[0]:#82が平均最速なら
     print('\n過去24時間で「サーバ82」が一番応答速度が速いので優先接続されます。')
     datalist2 = ['server 192.168.1.81 weight=1;\n server 192.168.1.82 weight=10;\n server 192.168.1.83 weight=1;\n']
     f.writelines(datalist2)
+    allocation='192.168.1.82'
     
-elif minmam == list3[0]:#82が平均最速なら
+elif minmam == list3[0]:#83が平均最速なら
     print('\n過去24時間で「サーバ83」が一番応答速度が速いので優先接続されます。')
     datalist2 = ['server 192.168.1.81 weight=1;\n server 192.168.1.82 weight=1;\n server 192.168.1.83 weight=10;\n']
     f.writelines(datalist2)
-
-
-"""
-if minmam == list1[0]:#81が平均最速なら
-    print('\n過去24時間で「サーバ81」が一番応答速度が速いので優先接続されます。')
-    datalist2 = ['server 192.168.1.81 weight=1;\n']
-    f.writelines(datalist2)
-else:
-    datalist2 = ['server 192.168.1.81 weight=10;\n']
-    f.writelines(datalist2)
+    allocation='192.168.1.83'
     
-if minmam == list2[0]:#82が平均最速なら
-    print('\n過去24時間で「サーバ82」が一番応答速度が速いので優先接続されます。')
-    datalist3 = ['server 192.168.1.82 weight=1;\n']
-    f.writelines(datalist3)
-else:
-    datalist3 = ['server 192.168.1.82 weight=10;\n']
-    f.writelines(datalist3)
-
-if minmam == list3[0]:#83が平均最速なら
-    print('\n過去24時間で「サーバ83」が一番応答速度が速いので優先接続されます。')
-    datalist4 = ['server 192.168.1.83 weight=1;\n']
-    f.writelines(datalist4)
-else:
-    datalist4 = ['server 192.168.1.83 weight=10;\n']
-    f.writelines(datalist4)
-"""
-    
-
 datalist5 = ['}\n','server{\n','listen 80;\n','server_name localhost;\n','location /{\n','proxy_pass http://backend1;\n','}\n','}\n','}\n']
 
 #コンフィグファイルへ保存
@@ -112,8 +88,37 @@ f.writelines(datalist5)
 f.close()
 
 
-#ロードバランサに処理を適用させる。（reload実行の為シェルを呼び出す）
-subprocess.run(['/home/pi/tools/nginxrestart.sh'])
+
+#--------------NGINX適応処理----------------
+
+#そのまま適応させると割り振り先カウントがリセットされる。前回と違った時だけ更新する。
+
+#前回の割り振りを呼び出し
+f2 = open('allocation.txt', 'r', encoding='UTF-8')
+data = f2.read()
+
+if allocation==data:
+    print('最速サーバは前回と同じ：設定変更ありません')
 
 
+#前回と違う→ロードバランサに処理を適用させる。（reload実行の為シェルを呼び出す）
+else:
+    print('最速サーバが変わりました：設定変更します')
+    subprocess.run(['/home/pi/tools/nginxrestart.sh'])
+    print('設定を変更しました')
+    
+    
+#--------------今回の割り振り先を記録----------------
 
+f2 = open('allocation.txt', 'w', encoding='UTF-8')
+
+if minmam == list1[0]:#81が平均最速なら
+    f2.write('192.168.1.81')
+    
+elif minmam == list2[0]:#82が平均最速なら
+    f2.write('192.168.1.82')
+    
+elif minmam == list3[0]:#83が平均最速なら
+    f2.write('192.168.1.83')
+
+f2.close()

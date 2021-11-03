46e3808af01797036f4454efa49874a104c63fe4
yusuke-matsuo <s1821086@cce.kanagawa-it.ac.jp>
2021-11-03 18:08:49 +0900

    11/03　質問、独自性のメモ、実験3回目失敗。次こそは成功させたい

---
+++ 実験/第3回自作LB実験（ネットワークトラブルで失敗）/実験用LB計測プログラム.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 00:31:47 2021
+
+@author: 1821086
+
+
+実験用：ロードバランサ計測プログラム
+requests.get()はHTTPのGETメソッドに相当する。
+応答以外にもページのスクレイピングも可能
+→どのWEBサーバへロードバランサが割り振ったのか確認できるので実験に最適。
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import time
+
+
+i = 0
+
+while i < 100000:
+    #ロードバランサの応答速度を計測
+    url = 'http://192.168.1.80/'
+    res = requests.get(url)
+    time_elapsed = res.elapsed.total_seconds()
+    print('time_elapsed:', time_elapsed)
+
+    # Webスクレイピング
+    html = requests.get(url)
+    soup = BeautifulSoup(html.content, "html.parser")
+    
+    # titleで判別（.textを指定することでタグの中を抽出できるらしい）
+    assign=soup.find("h1").text
+    print(assign)
+    
+    #時刻を取得
+    dt_now = datetime.datetime.now()
+    print(dt_now)
+    
+    #ファイルへ出力
+    f = open('LB_data_output.txt', 'a')
+    
+    #後でCSVで整理するとき便利だから区切り符を入れておく。
+    f.write(str(dt_now) + "/" + str(time_elapsed) + "/" + assign+"\n")
+    f.close()
+    i += 1
+    time.sleep(300)
+
+
+
+
+
+
+
+
+
+
+
